267bf86e473f64ec8836dbc449ad89a91fdbac0b
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2019-08-18 22:03:22 +0100

    Update clean.sh script

--- bin/clean.sh
+++ bin/clean.sh
@@ -4,7 +4,7 @@
 ./node_modules/docpad/bin/docpad clean
 
 # create folder for gh-pages branch
-mkdir ../2018.mobilization.pl-gh-pages
-cd ../2018.mobilization.pl-gh-pages
-git clone https://github.com/mobilization/2018.mobilization.pl.git .
+mkdir ../2019.mobilization.pl-gh-pages
+cd ../2019.mobilization.pl-gh-pages
+git clone https://github.com/mobilization/2019.mobilization.pl.git .
 git checkout gh-pages
